dc079a6614de9dd3b7d3668d025ef4783e9fd805
AyToFix <danilopitus@gmail.com>
2023-11-22 15:29:55 +0200

    add first lab

---
+++ lab1_1.py
@@ -0,0 +1,15 @@
+animal = "cat"
+print("favorite animal:", animal)
+
+is_weekend = False
+is_mammal = True
+has_wings = True
+
+result1 = is_weekend and is_mammal and has_wings
+print(result1)
+
+is_mammal = True
+has_wings = True
+
+result2 = is_mammal and has_wings
+print(result2)
